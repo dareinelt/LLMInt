2d23d16ccbd2058bde308eedd659702f5eb99073
dareinelt <dr@sportfotos-bs.de>
2026-08-10 07:06:45 +0200

    Change admin page title to 'Admin'

--- admin/index.php
+++ admin/index.php
@@ -1369,7 +1369,7 @@ if (isset($_GET['edit']) && (int) $_GET['edit'] > 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin – LM Studio Chat</title>
+    <title>Admin</title>
     <style>
         *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
 
